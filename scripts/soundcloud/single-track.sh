968dc33e1fc1173ca491f81703998af981050cae
BakerStreetBoys <alexbrown990909@gmail.com>
2026-04-29 18:52:19 +0330

    Create single-track.sh
    Add soundcloud single-track script

---
+++ scripts/soundcloud/single-track.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Load config
+source config/soundcloud.conf
+
+# Get URL from first argument
+URL="$1"
+
+# Create download directory
+mkdir -p "$DOWNLOAD_PATH"
+
+# Change to download directory
+cd "$DOWNLOAD_PATH"
+
+# Download single track as MP3
+yt-dlp --extract-audio --audio-format "$AUDIO_FORMAT" \
+  --output "%(uploader)s - %(title)s.%(ext)s" \
+  "$URL"
+
+echo "Single track download completed"
