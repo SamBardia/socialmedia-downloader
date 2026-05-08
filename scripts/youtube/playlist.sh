6704335dc47558764b5264f55334ea094949f4da
BakerStreetBoys <alexbrown990909@gmail.com>
2026-05-08 04:51:43 +0330

    Update playlist.sh

--- scripts/youtube/playlist.sh
+++ scripts/youtube/playlist.sh
@@ -7,7 +7,7 @@ if [ -f "config/youtube.conf" ]; then
     source "config/youtube.conf"
 fi
 
-DOWNLOAD_PATH="${DOWNLOAD_PATH:-downloads}"
+DOWNLOAD_PATH="${DOWNLOAD_PATH:-downloads/youtube}"
 URL="$1"
 QUALITY="${2:-480p}"
 
